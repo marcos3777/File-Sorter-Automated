83b5f40921660d151a0c70ad301f8a9fcdd2da59
marcos3777 <marcos_vinicius_11@hotmail.com>
2024-10-02 16:38:38 -0300

    Feito alguns ajustes finais nas pastas.

--- main.py
+++ main.py
@@ -1,53 +1,96 @@
-import os, shutil 
-import time 
-import sys
+import os
+import shutil
 import logging
+from posixpath import splitext
 from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
+from watchdog.events import FileSystemEventHandler
+
+source_dir = "C:/Users/Thanatos/Downloads"
+dest_dir_torrent = "C:/Users/Thanatos/Downloads/torrent files"
+dest_dir_video = "C:/Users/Thanatos/Downloads/video files"
+dest_dir_image = "C:/Users/Thanatos/Downloads/img files"
+dest_dir_documents = "C:/Users/Thanatos/Downloads/doc files"
+dest_dir_code = "C:/Users/Thanatos/Downloads/code files"
+dest_exe = "C:/Users/Thanatos/Downloads/exe files"
+
+image_extensions = [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".svg", ".ico"]
+video_extensions = [".webm", ".mp4", ".avi", ".mov", ".mkv"]
+torrent_extensions = [".torrent"]
+document_extensions = [".doc", ".docx", ".pdf", ".xls", ".xlsx", ".ppt", ".pptx"]
+code_extensions = [".py", ".html", ".css", ".js", ".cpp", ".c", ".java", ".php", ".sql", ".json", ".xml", ".md", ".txt"]
+installer_extensions = [".exe", ".msi"]
+
+
+dest_dirs = [dest_dir_torrent, dest_dir_video, dest_dir_image, dest_dir_documents, dest_dir_code]
+for folder in dest_dirs:
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+def makeUnique(dest, name):
+    filename, extension = splitext(name)
+    counter = 1
+    while os.path.exists(os.path.join(dest, name)):
+        name = f"{filename}({str(counter)}){extension}"
+        counter += 1
+    return name
+
+def move_files(dest, entry, name):
+    dest_path = os.path.join(dest, name)
+    if os.path.exists(dest_path):
+        unique_name = makeUnique(dest, name)
+        dest_path = os.path.join(dest, unique_name)
+    shutil.move(entry.path, dest_path)
+    logging.info(f"Moved {entry.path} to {dest_path}")
+
+class MoveHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        with os.scandir(source_dir) as entries:
+            for entry in entries:
+                name = entry.name
+                if entry.is_file(): 
+                    self.check_torrent_files(entry, name)
+                    self.check_video_files(entry, name)
+                    self.check_image_files(entry, name)
+                    self.check_document_files(entry, name)
+                    self.check_code_files(entry, name)
+                    self.check_installer_files(entry, name)
+
+    def check_torrent_files(self, entry, name):
+        if any(name.endswith(ext) for ext in torrent_extensions):
+            move_files(dest_dir_torrent, entry, name)
+
+    def check_video_files(self, entry, name):
+        if any(name.endswith(ext) for ext in video_extensions):
+            move_files(dest_dir_video, entry, name)
+
+    def check_image_files(self, entry, name):
+        if any(name.endswith(ext) for ext in image_extensions):
+            move_files(dest_dir_image, entry, name)
+
+    def check_document_files(self, entry, name):
+        if any(name.endswith(ext) for ext in document_extensions):
+            move_files(dest_dir_documents, entry, name)
+
+    def check_code_files(self, entry, name):
+        if any(name.endswith(ext) for ext in code_extensions):
+            move_files(dest_dir_code, entry, name)
+
+    def check_installer_files(self, entry, name):
+        if any(name.endswith(ext) for ext in installer_extensions):
+            move_files(dest_exe, entry, name)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
-    path = sys.argv[1] if len(sys.argv) > 1 else '.'
-    event_handler = LoggingEventHandler()
+    patch = source_dir
+    event_handler = MoveHandler()
     observer = Observer()
-    observer.schedule(event_handler, path, recursive=True)
+    observer.schedule(event_handler, patch, recursive=True)
     observer.start()
     try:
-        while observer.isAlive():
+        while observer.is_alive():
             observer.join(1)
     finally:
         observer.stop()
         observer.join()
-
-path = r"C:/Users/Thanatos/Downloads/"
-
-os.listdir(path)
-
-file_name = os.listdir(path)
-
-folder_names = ['img files', 'doc files', 'torrent files', 'exe files', 'video files', 'code files']
-
-for loop in folder_names:
-    if not os.path.exists(path + loop):
-        os.mkdir(path + loop)
-
-for file in file_name:
-    if file.endswith('.jpg') or file.endswith('.png') or file.endswith('jpeg') and not os.path.exists(path + 'img files/' + file):
-        shutil.move(path + file, path + 'img files')
-    elif file.endswith('.doc') or file.endswith('.docx') or file.endswith('.pdf')  and not os.path.exists(path + 'doc files/' + file):
-        shutil.move(path + file, path + 'doc files')
-    elif file.endswith('.torrent'):
-        shutil.move(path + file, path + 'torrent files') and not os.path.exists(path + 'torrent files/' + file)
-    elif file.endswith('.exe') and not os.path.exists(path + 'exe files/' + file):
-        shutil.move(path + file, path + 'exe files')
-    elif file.endswith('.mp4') or file.endswith('.mkv') and not os.path.exists(path + 'video files/' + file):
-        shutil.move(path + file, path + 'video files')
-    elif file.endswith('.py') or file.endswith('.js') or file.endswith('.html') and not os.path.exists(path + 'code files/' + file):
-        shutil.move(path + file, path + 'code files')
-    else:
-        pass
-
-    #Até esse ponto o codigo esta funcionando normalmente. A partir daqui tentarei automatizar usando watchdog
-
